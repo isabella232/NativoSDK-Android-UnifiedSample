424918095e05959c85132fd073f027ba0be7a4b1
nnavakiran <nnavakiran@nativo.com>
2019-01-17 16:38:36 -0800

    Implement BOAP

--- app/src/main/java/com/nativo/nativo_android_unifiedsample/NativeAdLandingImpl/NativeLandingPage.java
+++ app/src/main/java/com/nativo/nativo_android_unifiedsample/NativeAdLandingImpl/NativeLandingPage.java
@@ -1,18 +1,27 @@
 package com.nativo.nativo_android_unifiedsample.NativeAdLandingImpl;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nativo.nativo_android_unifiedsample.R;
 
+import net.nativo.sdk.NativoSDK;
+import net.nativo.sdk.ntvadtype.NtvBaseInterface;
 import net.nativo.sdk.ntvadtype.landing.NtvLandingPageInterface;
+import net.nativo.sdk.ntvcore.NtvAdData;
+import net.nativo.sdk.ntvcore.NtvSectionAdapter;
 
 import java.util.Date;
+import java.util.Random;
 
-public class NativeLandingPage implements NtvLandingPageInterface {
+import static com.nativo.nativo_android_unifiedsample.util.AppConstants.SECTION_URL;
+
+public class NativeLandingPage implements NtvLandingPageInterface, NtvSectionAdapter {
 
     private WebView webView;
     private TextView titleLabel;
@@ -66,7 +75,7 @@ public class NativeLandingPage implements NtvLandingPageInterface {
 
     @Override
     public void contentWebViewOnPageFinished() {
-
+        NativoSDK.getInstance().prefetchAdForSection(SECTION_URL, new Random().nextInt(), this, null);
     }
 
     @Override
@@ -90,5 +99,46 @@ public class NativeLandingPage implements NtvLandingPageInterface {
     @Override
     public View getAdContainerView() {
         return adContainerView;
+    }
+
+    @Override
+    public boolean shouldPlaceAdAtIndex(String s, int i) {
+        return true;
+    }
+
+    @Override
+    public Class<?> registerLayoutClassForIndex(int i, NtvAdData.NtvAdTemplateType ntvAdTemplateType) {
+        return null;
+    }
+
+    @Override
+    public void needsDisplayLandingPage(String s, int i) {
+
+    }
+
+    @Override
+    public void needsDisplayClickOutURL(String s, String s1) {
+
+    }
+
+    @Override
+    public void hasbuiltView(View view, NtvBaseInterface ntvBaseInterface, NtvAdData ntvAdData) {
+
+    }
+
+    @Override
+    public void onReceiveAd(String s, int i, NtvAdData ntvAdData) {
+        tryPlaceAd(i);
+    }
+
+    private void tryPlaceAd(int i) {
+        View view = adContainerView.findViewById(R.id.article_layout);
+        ViewGroup viewGroup = adContainerView.findViewById(R.id.landing_boap_container);
+        NativoSDK.getInstance().placeAdInView(view, viewGroup, SECTION_URL, i, this, null);
+    }
+
+    @Override
+    public void onFail(String s, int i) {
+        tryPlaceAd(i);
     }
 }
