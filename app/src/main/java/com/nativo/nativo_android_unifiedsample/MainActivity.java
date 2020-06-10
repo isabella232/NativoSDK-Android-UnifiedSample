d7633e53f617cb9047dc7e9be94cd7a795e541fd
nnavakiran <nnavakiran@nativo.com>
2020-06-10 10:49:15 -0600

    Update for latest GAM and add support for GDPR menu options

--- app/src/main/java/com/nativo/nativo_android_unifiedsample/MainActivity.java
+++ app/src/main/java/com/nativo/nativo_android_unifiedsample/MainActivity.java
@@ -4,8 +4,15 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
 import com.google.android.material.tabs.TabLayout;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -28,8 +35,10 @@ import com.nativo.sampleapp.R;
 
 import net.nativo.sdk.NativoSDK;
 
+import static com.nativo.nativo_android_unifiedsample.util.AppConstants.SAMPLE_CCPA_INVALID_CONSENT;
 import static com.nativo.nativo_android_unifiedsample.util.AppConstants.SAMPLE_CCPA_VALID_CONSENT;
 import static com.nativo.nativo_android_unifiedsample.util.AppConstants.SAMPLE_GDPR_CONSENT;
+import static com.nativo.nativo_android_unifiedsample.util.AppConstants.SAMPLE_GDPR_INVALID_CONSENT;
 import static net.nativo.sdk.ntvconstant.NtvConstants.CCPA_SHARED_PREFERENCE_STRING;
 import static net.nativo.sdk.ntvconstant.NtvConstants.GDPR_SHARED_PREFERENCE_STRING;
 
@@ -45,13 +54,11 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setOffscreenPageLimit(0);
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-        setPrivacyAndTransparencyKeys();
         init();
     }
 
     private void setPrivacyAndTransparencyKeys() {
-        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+        SharedPreferences.Editor editor = getEditor();
         editor.putString(GDPR_SHARED_PREFERENCE_STRING, SAMPLE_GDPR_CONSENT);
         editor.putString(CCPA_SHARED_PREFERENCE_STRING, SAMPLE_CCPA_VALID_CONSENT);
         editor.apply();
@@ -142,5 +149,49 @@ public class MainActivity extends AppCompatActivity {
             }
             return null;
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.yes:
+                setPrivacyAndTransparencyKeys();
+                return true;
+            case R.id.no:
+                removePrivacyAndTransparencyKeys();
+                return true;
+            case R.id.invalid:
+                invalidPrivacyAndTransparencyKeys();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void invalidPrivacyAndTransparencyKeys() {
+        SharedPreferences.Editor editor = getEditor();
+        editor.putString(GDPR_SHARED_PREFERENCE_STRING, SAMPLE_GDPR_INVALID_CONSENT);
+        editor.putString(CCPA_SHARED_PREFERENCE_STRING, SAMPLE_CCPA_INVALID_CONSENT);
+        editor.apply();
+
+    }
+
+    private SharedPreferences.Editor getEditor() {
+        SharedPreferences sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        return sharedPreferences.edit();
+    }
+
+    private void removePrivacyAndTransparencyKeys() {
+        SharedPreferences.Editor editor = getEditor();
+        editor.clear();
+        editor.apply();
     }
 }
--- app/src/main/java/com/nativo/nativo_android_unifiedsample/ViewFragment/DfpFragment.java
+++ app/src/main/java/com/nativo/nativo_android_unifiedsample/ViewFragment/DfpFragment.java
@@ -46,6 +46,7 @@ public class DfpFragment extends Fragment implements NtvSectionAdapter {
     String mMesssage;
     DfpFragment fragmentAdapter;
     View nativoView;
+    View nativoVideoView;
     ViewGroup parentView;
 
     public DfpFragment() {
@@ -67,7 +68,9 @@ public class DfpFragment extends Fragment implements NtvSectionAdapter {
         super.onViewCreated(view, savedInstanceState);
         parentView = (ViewGroup) view;
         nativoView = view.findViewById(R.id.article_constraint_layout);
-        nativoView.setVisibility(View.INVISIBLE);
+        nativoView.setVisibility(View.GONE);
+        nativoVideoView = view.findViewById(R.id.video_constraint_layout);
+        nativoVideoView.setVisibility(View.GONE);
         NativoSDK.getInstance().enableDFPRequestsWithVersion("17.0.0");
         View loadAd = view.findViewById(R.id.load_ad);
         loadAd.setOnClickListener(loadClick);
@@ -137,8 +140,13 @@ public class DfpFragment extends Fragment implements NtvSectionAdapter {
     @Override
     public void onReceiveAd(String s, NtvAdData ntvAdData) {
         Log.d("DFP", "Ad loaded");
-        nativoView.setVisibility(View.VISIBLE);
-        NativoSDK.getInstance().placeAdInView(nativoView, parentView, DFP_SECTION_URL, 0, fragmentAdapter, null);
+        if (ntvAdData.getAdType() == NtvAdData.NtvAdType.NATIVE || ntvAdData.getAdType() == NtvAdData.NtvAdType.CLICK_OUT) {
+            nativoView.setVisibility(View.VISIBLE);
+            NativoSDK.getInstance().placeAdInView(nativoView, parentView, DFP_SECTION_URL, 0, fragmentAdapter, null);
+        } else if (ntvAdData.getAdType() == NtvAdData.NtvAdType.IN_FEED_VIDEO || ntvAdData.getAdType() == NtvAdData.NtvAdType.IN_FEED_AUTO_PLAY_VIDEO) {
+            nativoVideoView.setVisibility(View.VISIBLE);
+            NativoSDK.getInstance().placeAdInView(nativoVideoView, parentView, DFP_SECTION_URL, 0, fragmentAdapter, null);
+        }
     }
 
     @Override
